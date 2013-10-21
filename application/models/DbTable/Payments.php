0a45a75bf8f04d2c960f3b987adfb90e089e8ba5
Paul <ipaul.v@gmail.com>
2013-10-21 11:57:09 +0300

    my version

--- application/models/DbTable/Payments.php
+++ application/models/DbTable/Payments.php
@@ -5,6 +5,25 @@ class Application_Model_DbTable_Payments extends Zend_Db_Table_Abstract
 
     protected $_name = 'payments';
 
+    public function getPayments($id)
+    {
+        $select = $this->select();
+        if (!$select) {
+            throw new Exception('Нет платежей от этого студента');
+        }
+        $select->where('idStudent =' . $id);
+        return $this->fetchAll($select);
+    }
+
+    public function addPayment($data)
+    {
+        return $this->insert($data);
+    }
+
+    public function editPayment($id, $data)
+    {
+        return $this->update($data, 'id='.(int)$id);
+    }
 
 }
 
